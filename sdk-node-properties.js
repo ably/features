679825f03f6620a80241885f06f6f81426f72183
Quintin Willison <q@qwuk.net>
2022-09-30 17:52:48 +0100

    Extract string transformation functions into their own module and add tests for them.

--- sdk-node-properties.js
+++ sdk-node-properties.js
@@ -1,5 +1,11 @@
 const escape = require('escape-html');
 
+const {
+  IDENTITY_TRANSFORM,
+  transformString,
+  transformStrings,
+} = require('./transform');
+
 const isPropertyKey = (key) => key.startsWith('.');
 const propertyKeyName = (key) => key.substring(1);
 
@@ -22,8 +28,6 @@ class SpecificationPoint {
     return `<a href="${url}" target="_blank" rel="noopener"><code>${escape(this.value)}</code></a>`;
   }
 }
-
-const IDENTITY_TRANSFORM = (value) => value;
 
 class Properties {
   constructor(node) {
@@ -77,46 +81,3 @@ module.exports = {
   Properties,
   SpecificationPoint,
 };
-
-/**
- * Callback transforming a string.
- *
- * @callback StringTransformer
- * @param {string} value The string to be transformed.
- * @returns {*} The result of transforming the string.
- */
-
-/**
- * Creates a new array populated with the results of calling a provided function with one
- * or more string values.
- *
- * @param {string|string[]} value A single string, or an array of strings.
- * @param {StringTransformer} transformer A function to be called with each string.
- * @returns {*[]} The results of transforming the string(s).
- * @throws If no values were provided or some values were not strings.
- */
-function transformStrings(value, transformer) {
-  if (value == null) {
-    throw new Error('The value may not be null or undefined.');
-  }
-  const array = Array.isArray(value) ? value : [value];
-  if (array.length < 1) {
-    throw new Error('No values to transform.');
-  }
-  return array.map((element) => transformString(element, transformer));
-}
-
-/**
- * Returns the result of calling a provided function with a string value.
- *
- * @param {string} value A single string.
- * @param {StringTransformer} transformer A function to be called with the string.
- * @returns {*} The result of transforming the string.
- * @throws If the value provided is not a string.
- */
-function transformString(value, transformer) {
-  if (value instanceof String || typeof value === 'string') {
-    return transformer(value);
-  }
-  throw new Error(`Encountered '${typeof value}' (${value}) when expecting a string.`);
-}
---
+++ transform.test.js
@@ -0,0 +1,83 @@
+const {
+  IDENTITY_TRANSFORM,
+  transformString,
+  transformStrings,
+} = require('./transform');
+
+test('IDENTITY_TRANSFORM', () => {
+  expect(IDENTITY_TRANSFORM(0)).toBe(0);
+  expect(IDENTITY_TRANSFORM(666)).toBe(666);
+  expect(IDENTITY_TRANSFORM(null)).toBe(null);
+  expect(IDENTITY_TRANSFORM(undefined)).toBe(undefined);
+  expect(IDENTITY_TRANSFORM('hello')).toBe('hello');
+  expect(IDENTITY_TRANSFORM('')).toBe('');
+  expect(IDENTITY_TRANSFORM(' ')).toBe(' ');
+});
+
+const intTransformer = (value) => parseInt(value, 10);
+
+describe('transformString', () => {
+  it('successfully identity transforms', () => {
+    expect(transformString('', IDENTITY_TRANSFORM)).toBe('');
+    expect(transformString('hello', IDENTITY_TRANSFORM)).toBe('hello');
+  });
+
+  it('successfully custom transforms', () => {
+    expect(transformString('123', intTransformer)).toBe(123);
+    expect(transformString('0', intTransformer)).toBe(0);
+    expect(transformString('-1', intTransformer)).toBe(-1);
+    expect(transformString('hello', intTransformer)).toBeNaN();
+  });
+
+  it('fails for non-string values', () => {
+    expect(() => { transformString(0, IDENTITY_TRANSFORM); }).toThrow();
+    expect(() => { transformString(null, IDENTITY_TRANSFORM); }).toThrow();
+    expect(() => { transformString(undefined, IDENTITY_TRANSFORM); }).toThrow();
+    expect(() => { transformString([], IDENTITY_TRANSFORM); }).toThrow();
+    expect(() => { transformString(['hello'], IDENTITY_TRANSFORM); }).toThrow();
+    expect(() => { transformString({}, IDENTITY_TRANSFORM); }).toThrow();
+    expect(() => { transformString({ hello: 'world' }, IDENTITY_TRANSFORM); }).toThrow();
+  });
+});
+
+describe('transformStrings', () => {
+  describe('for string arguments', () => {
+    it('successfully identity transforms', () => {
+      expect(transformStrings('', IDENTITY_TRANSFORM)).toStrictEqual(['']);
+      expect(transformStrings('hello', IDENTITY_TRANSFORM)).toStrictEqual(['hello']);
+    });
+
+    it('successfully custom transforms', () => {
+      expect(transformStrings('123', intTransformer)).toStrictEqual([123]);
+      expect(transformStrings('0', intTransformer)).toStrictEqual([0]);
+      expect(transformStrings('-1', intTransformer)).toStrictEqual([-1]);
+      expect(transformStrings('hello', intTransformer)).toStrictEqual([NaN]);
+    });
+  });
+
+  describe('for string array arguments', () => {
+    it('successfully identity transforms', () => {
+      expect(transformStrings([''], IDENTITY_TRANSFORM)).toStrictEqual(['']);
+      expect(transformStrings(['hello'], IDENTITY_TRANSFORM)).toStrictEqual(['hello']);
+      expect(transformStrings(['a', 'b', 'c'], IDENTITY_TRANSFORM)).toStrictEqual(['a', 'b', 'c']);
+    });
+
+    it('successfully custom transforms', () => {
+      expect(transformStrings(['123'], intTransformer)).toStrictEqual([123]);
+      expect(transformStrings(['0'], intTransformer)).toStrictEqual([0]);
+      expect(transformStrings(['-1'], intTransformer)).toStrictEqual([-1]);
+      expect(transformStrings(['1', '2', '3'], intTransformer)).toStrictEqual([1, 2, 3]);
+      expect(transformStrings(['hello'], intTransformer)).toStrictEqual([NaN]);
+    });
+  });
+
+  it('fails for non-string or non-string-array values', () => {
+    expect(() => { transformStrings(0, IDENTITY_TRANSFORM); }).toThrow();
+    expect(() => { transformStrings(null, IDENTITY_TRANSFORM); }).toThrow();
+    expect(() => { transformStrings(undefined, IDENTITY_TRANSFORM); }).toThrow();
+    expect(() => { transformStrings([123], IDENTITY_TRANSFORM); }).toThrow();
+    expect(() => { transformStrings(['hello', 456], IDENTITY_TRANSFORM); }).toThrow();
+    expect(() => { transformStrings({}, IDENTITY_TRANSFORM); }).toThrow();
+    expect(() => { transformStrings({ hello: 'world' }, IDENTITY_TRANSFORM); }).toThrow();
+  });
+});
